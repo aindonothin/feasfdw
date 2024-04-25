2c56023852519aa23200e81b496f1930eac46873
aindonothin <143459897+aindonothin@users.noreply.github.com>
2024-04-25 16:00:00 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -71,7 +71,7 @@ let last_combat_location = null;
 let is_reading = null;
 
 //ticks between saves, 60 = ~1 minute
-let save_period = 60;
+let save_period = 1200;
 let save_counter = 0;
 
 //accumulates deviations
