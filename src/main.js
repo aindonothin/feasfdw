33887173c7a13c6fe8efee362a47841def29943e
aindonothin <143459897+aindonothin@users.noreply.github.com>
2024-04-25 17:35:31 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -89,7 +89,7 @@ let selected_stance = "normal";
 let current_stance = "normal";
 const faved_stances = {};
 
-const tickrate = 1; 
+var tickrate = 1; 
 
 //how many ticks per second
 //1 is the default value; going too high might make the game unstable
