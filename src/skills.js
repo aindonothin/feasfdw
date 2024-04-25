3b019206feeb24798567d5427cfe39175f0412fd
aindonothin <143459897+aindonothin@users.noreply.github.com>
2024-04-25 19:07:21 -0300

    Update skills.js

--- src/skills.js
+++ src/skills.js
@@ -1065,10 +1065,10 @@ Multiplies attack speed in unarmed combat by ${Math.round((skills["Unarmed"].get
     skills["Farming"] = new Skill({skill_id: "Farming", 
                                 names: {0: "Farming"}, 
                                 description: "Even a simple action of plowing some fields, can be performed better with skills and experience",
-                                base_xp_cost: 40,
+                                base_xp_cost: 4,
                                 max_level: 250,
                                 xp_scaling: 1,
-                                max_level_coefficient: 2,
+                                max_level_coefficient: 1.1,
                                 rewards: {
                                     milestones: {
                                         1: {
